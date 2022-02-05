40b173e4c2230d08a10b91d556056c4364108eb3
cooperingot <99025957+cooperingot@users.noreply.github.com>
2022-02-05 20:16:05 +0800

    Update mathfox.js

--- mation/mathfox.js
+++ mation/mathfox.js
@@ -1,10 +1,15 @@
 var elementTimes = 1;
-function showButtonMenu(){
-	document.body.style.backgroundColor = "black";
+function deleteSelf(){
+    var self = window.event.srcElement;
+    self.style.animation = "delete 0.3s 1";
+    setTimeout(function(){self.remove();},300);
 }
 function createButton(){
 	elementTimes++;
 	var obj = document.createElement("div");
+    obj.style.animation = "display 0.3s 1";
+	obj.id = elementTimes;
+	obj.ondblclick = function(){deleteSelf();};
 	obj.className = "elementButton";
 	obj.innerHTML = "元素" + elementTimes;
 	document.getElementById("titleLabel").appendChild(obj);
